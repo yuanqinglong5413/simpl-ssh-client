aad4ba6cd7706601366577718525537c46c2bbe5
jumao <2893462355@qq.com>
2026-06-27 13:17:38 +0800

    chore: 同步版本号到 v0.9.0 (tauri.conf / Cargo.toml / package.json / StatusBar)

--- src/components/StatusBar.tsx
+++ src/components/StatusBar.tsx
@@ -90,7 +90,7 @@ export function StatusBar({
             ⌘K
           </button>
         )}
-        <span>simpl-ssh v0.8.2</span>
+        <span>simpl-ssh v0.9.0</span>
       </div>
     </div>
   );
